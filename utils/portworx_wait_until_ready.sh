115b4ac9c3625ebf86cc90610e83017a4b1c5c64
sudas-px <109576118+sudas-px@users.noreply.github.com>
2022-09-01 19:02:43 +0530

    Update portworx_wait_until_ready.sh

--- utils/portworx_wait_until_ready.sh
+++ utils/portworx_wait_until_ready.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#TODO: fix this sleep, sh error `]` missing
 echo '**********************************************************************'
 echo '            Validating Portworx Enterprise Installation'
 echo '**********************************************************************'
@@ -34,7 +33,7 @@ while [ "$RETRIES" -le "$LIMIT" ]; do
   elif [ "$DESIRED" -eq "$READY" ]; then
     echo "[INFO] ($READY/$DESIRED) Pods Ready..."
     break
-  elif [ "$DESIRED" -gt "$READY "]; then
+  elif [ "$DESIRED" -gt "$READY" ]; then
     echo "[INFO] ($READY/$DESIRED) Pods Ready, waiting for pods to come up..."
     sleep $SLEEP_TIME
   fi
