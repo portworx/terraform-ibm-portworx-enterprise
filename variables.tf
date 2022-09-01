128857707f1ca02a49d1540679aef0667d9da32d
Sumit Das <sudas@purestorage.com>
2022-09-01 17:51:28 +0530

    Updated Examples

--- variables.tf
+++ variables.tf
@@ -1,26 +1,4 @@
 //TODO: Add Descriptions
-variable "region" {
-  type        = string
-  description = "(optional) describe your variable"
-  default     = "us-east"
-}
-
-variable "iks_cluster_name" {
-  type        = string
-  description = "(optional) describe your variable"
-}
-
-variable "resource_group" {
-  type        = string
-  description = "(optional) describe your variable"
-}
-
-variable "ibmcloud_api_key" {
-  description = "Get the ibmcloud api key from https://cloud.ibm.com/iam/apikeys"
-  type        = string
-  sensitive   = true
-}
-
 variable "ibmcloud_api_key" {
   description = "Get the ibmcloud api key from https://cloud.ibm.com/iam/apikeys"
   type        = string
