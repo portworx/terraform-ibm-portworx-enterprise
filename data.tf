3ff69afc196bb97ffeaa84b52ad5960a484bd281
Sumit Das <sudas@purestorage.com>
2022-09-06 23:52:13 +0530

    Support for Classic Infra

--- data.tf
+++ data.tf
@@ -11,9 +11,9 @@ data "ibm_container_vpc_cluster" "cluster" {
 }
 
 data "ibm_container_vpc_cluster_worker" "worker" {
-  count             = var.classic_infra ? 0 : length(data.ibm_container_vpc_cluster.cluster.workers)
-  worker_id         = element(data.ibm_container_vpc_cluster.cluster.workers, count.index)
-  cluster_name_id   = data.ibm_container_vpc_cluster.cluster.id
+  count             = var.classic_infra ? 0 : length(data.ibm_container_vpc_cluster.cluster[0].workers)
+  worker_id         = element(data.ibm_container_vpc_cluster.cluster[0].workers, count.index)
+  cluster_name_id   = data.ibm_container_vpc_cluster.cluster[0].id
   resource_group_id = data.ibm_resource_group.group.id
 }
 
@@ -24,8 +24,8 @@ data "ibm_container_cluster" "cluster_classic" {
 }
 
 data "ibm_container_cluster_worker" "worker_classic" {
-  count             = var.classic_infra ? length(data.ibm_container_cluster.cluster_classic.workers) : 0
-  worker_id         = element(data.ibm_container_cluster.cluster_classic.workers, count.index)
-  cluster_name_id   = data.ibm_container_cluster.cluster_classic.id
+  count             = var.classic_infra ? length(data.ibm_container_cluster.cluster_classic[0].workers) : 0
+  worker_id         = element(data.ibm_container_cluster.cluster_classic[0].workers, count.index)
+  cluster_name_id   = data.ibm_container_cluster.cluster_classic[0].id
   resource_group_id = data.ibm_resource_group.group.id
 }
