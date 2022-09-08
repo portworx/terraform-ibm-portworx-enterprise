3e7710151f0f4237c5e318284cbfc5515906f654
Sumit Das <sudas@purestorage.com>
2022-09-08 15:41:36 +0530

    refactored `main.tf` using templates

--- data.tf
+++ data.tf
@@ -18,8 +18,8 @@ data "ibm_container_vpc_cluster_worker" "worker" {
 }
 
 data "ibm_container_cluster" "cluster_classic" {
-  count = var.classic_infra ? 1 : 0
-  name = var.cluster_name
+  count             = var.classic_infra ? 1 : 0
+  name              = var.cluster_name
   resource_group_id = data.ibm_resource_group.group.id
 }
 
@@ -27,4 +27,28 @@ data "ibm_container_cluster_worker" "worker_classic" {
   count             = var.classic_infra ? length(data.ibm_container_cluster.cluster_classic[0].workers) : 0
   worker_id         = element(data.ibm_container_cluster.cluster_classic[0].workers, count.index)
   resource_group_id = data.ibm_resource_group.group.id
+}
+
+locals {
+  cluster_ref = var.classic_infra ? data.ibm_container_cluster.cluster_classic[0] : data.ibm_container_vpc_cluster.cluster[0]
+  params = {
+    apikey                    = var.ibmcloud_api_key,
+    cluster_name              = var.cluster_name,
+    clusters                  = var.cluster_name,
+    etcd_endpoint             = var.use_external_etcd ? external_etcd_connection_url : null,
+    etcd_secret               = var.use_external_etcd ? var.etcd_secret_name : null,
+    internal_kvdb             = var.use_external_etcd ? "external" : "internal",
+    image_version             = var.portworx_version,
+    secret_type               = var.secret_type,
+    csi                       = var.csi ? "True" : "False",
+    use_cloud_drives          = var.use_cloud_drives ? "Yes" : "No",
+    max_storage_node_per_zone = var.max_storage_node_per_zone,
+    num_cloud_drives          = var.num_cloud_drives,
+    size                      = element(var.cloud_drives_sizes, 0),
+    size2                     = (var.num_cloud_drives == 2) ? element(var.cloud_drives_sizes, 1) : 0,
+    size3                     = (var.num_cloud_drives == 3) ? element(var.cloud_drives_sizes, 2) : 0,
+    storageClassName          = element(var.storage_classes, 0),
+    storageClassName2         = (var.num_cloud_drives == 2) ? element(var.storage_classes, 1) : "",
+    storageClassName3         = (var.num_cloud_drives == 3) ? element(var.storage_classes, 2) : ""
+  }
 }
--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 resource "random_uuid" "unique_id" {
 }
-
 resource "ibm_resource_instance" "portworx" {
-  count             = var.classic_infra ? 0 : 1
   name              = "portworx-service-${split("-", random_uuid.unique_id.result)[0]}"
   service           = "portworx"
   plan              = var.pwx_plan
@@ -10,81 +8,21 @@ resource "ibm_resource_instance" "portworx" {
   resource_group_id = data.ibm_resource_group.group.id
 
   tags = [
-    "clusterid:${data.ibm_container_vpc_cluster.cluster[0].id}",
+    "clusterid:${local.cluster_ref.id}",
     "managed_by:terraform",
-    "cluster_name:${data.ibm_container_vpc_cluster.cluster[0].name}",
+    "cluster_name:${local.cluster_ref.name}",
     "owner:sudas"
   ]
   //TODO: Recheck Tags
-
-  parameters = {
-    apikey                    = var.ibmcloud_api_key
-    cluster_name              = var.cluster_name
-    clusters                  = var.cluster_name
-    etcd_endpoint             = var.use_external_etcd ? var.external_etcd_connection_url : null
-    etcd_secret               = var.use_external_etcd ? var.etcd_secret_name : null
-    internal_kvdb             = var.use_external_etcd ? "external" : "internal"
-    image_version             = var.portworx_version
-    secret_type               = var.secret_type
-    cloud_drive               = var.use_cloud_drives ? "Yes" : "No"
-    max_storage_node_per_zone = var.max_storage_node_per_zone
-    num_cloud_drives          = var.num_cloud_drives
-    size                      = element(var.cloud_drives_sizes, 0)
-    size2                     = (var.num_cloud_drives == 2) ? element(var.cloud_drives_sizes, 1) : 0
-    size3                     = (var.num_cloud_drives == 3) ? element(var.cloud_drives_sizes, 2) : 0
-    storageClassName          = element(var.storage_classes, 0)
-    storageClassName2         = (var.num_cloud_drives == 2) ? element(var.storage_classes, 1) : ""
-    storageClassName3         = (var.num_cloud_drives == 3) ? element(var.storage_classes, 2) : ""
-    csi                       = var.csi ? "True" : "False"
-    //TODO: fix csi boolean issue
-  }
+  parameters = templatefile("${path.module}/parameters.tftpl", local.params)
+  //TODO: fix csi boolean issue
+  
 
   provisioner "local-exec" {
     working_dir = "${path.module}/utils/"
     command     = "/bin/bash portworx_wait_until_ready.sh"
   }
 }
-
-
-
-
-resource "ibm_resource_instance" "portworx_on_classic" {
-  count             = var.classic_infra ? 1 : 0
-  name              = "portworx-service-${split("-", random_uuid.unique_id.result)[0]}"
-  service           = "portworx"
-  plan              = var.pwx_plan
-  location          = var.region
-  resource_group_id = data.ibm_resource_group.group.id
-
-  tags = [
-    "clusterid:${data.ibm_container_cluster.cluster_classic[0].id}",
-    "managed_by:terraform",
-    "cluster_name:${data.ibm_container_cluster.cluster_classic[0].name}",
-    "owner:sudas"
-  ]
-  //TODO: Recheck Tags
-
-  parameters = {
-    apikey                    = var.ibmcloud_api_key
-    cluster_name              = var.cluster_name
-    clusters                  = var.cluster_name
-    etcd_endpoint             = var.use_external_etcd ? var.external_etcd_connection_url : null
-    etcd_secret               = var.use_external_etcd ? var.etcd_secret_name : null
-    internal_kvdb             = var.use_external_etcd ? "external" : "internal"
-    image_version             = var.portworx_version
-    secret_type               = var.secret_type
-    csi                       = var.csi ? "True" : "False"
-    //TODO: fix csi boolean issue
-  }
-
-  provisioner "local-exec" {
-    working_dir = "${path.module}/utils/"
-    command     = "/bin/bash portworx_wait_until_ready.sh"
-  }
-}
-
-
-
 
 resource "null_resource" "portworx_destroy" {
   provisioner "local-exec" {
