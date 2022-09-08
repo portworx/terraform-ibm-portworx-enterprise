3a8d1b2869fae468b1227986716acf264afa30e0
Sumit Das <sudas@purestorage.com>
2022-09-08 18:25:20 +0530

    fix: outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "portworx_is_ready" {
-  value = var.classic_infra ? ibm_resource_instance.portworx_on_classic[0].id : ibm_resource_instance.portworx[0].id
+  value = ibm_resource_instance.portworx.id 
 }
