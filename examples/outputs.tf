8e3c8fbef2b99d8f21c540afa28daa50bc2ed70f
Sumit Das <sudas@purestorage.com>
2022-09-12 13:14:55 +0530

    update: examples

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -1,3 +1,15 @@
-output "portworx_is_ready" {
-  value = module.portworx-enterprise.portworx_is_ready
+output "portworx_enterprise_id" {
+  value = module.portworx-enterprise.portworx_enterprise_id
+}
+output "portworx_enterprise_service_name" {
+  value = module.portworx-enterprise.portworx_enterprise_service_name
+}
+output "portworx_version_installed" {
+  value = module.portworx-enterprise.portworx_version_installed
+}
+output "associated_iks_cluster_id" {
+  value = module.portworx-enterprise.associated_iks_cluster_id
+}
+output "associated_iks_cluster_name" {
+  value = module.portworx-enterprise.associated_iks_cluster_name
 }
