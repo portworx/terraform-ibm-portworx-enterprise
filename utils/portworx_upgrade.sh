98e1d33f16dedf6fe674b24c818b1756337484c9
Sumit Das <sudas@purestorage.com>
2022-09-23 23:34:56 +0530

    fix: upgrade script
    Signed-off-by: Sumit Das <sudas@purestorage.com>

--- utils/portworx_upgrade.sh
+++ utils/portworx_upgrade.sh
@@ -2,15 +2,50 @@
 IMAGE_VERSION=$1
 UPGRADE_REQUESTED=$2
 
+DIVIDER="\n*************************************************************************\n"
+HEADER="$DIVIDER*\t\tUpgrade Requested to Portworx Enterprise ${IMAGE_VERSION}\t\t*$DIVIDER"
+
+DESIRED=0
+READY=0
+
 if $UPGRADE_REQUESTED
 then
-    echo '**********************************************************************'
-    echo "     Upgrade Requested to Portworx Enterprise ${IMAGE_VERSION}"
-    echo '**********************************************************************'
+    printf $HEADER
 else
-    echo "No Upgrade Requested!!"
+    printf "No Upgrade Requested!!\n"
     exit 0
 fi
+
+if ! ds_state=($( kubectl get -n kube-system ds portworx -o jsonpath='{.status.desiredNumberScheduled} {.status.numberReady}')); then
+    printf "[ERROR] Portworx Daemon Set Not Found, will not proceed with the upgrade!! Please install portworx and then try to upgrade.\n"
+    exit 1
+else
+    ds_status=($(kubectl describe ds portworx -n kube-system | grep "Pods Status" | cut -d ":" -f 2))
+    DESIRED="${ds_state[0]}"
+    READY="${ds_state[1]}"
+    printf "$HEADER*\t\t\t\tDaemonset Status\t\t\t*\n* portworx\t[ ${ds_status[*]} ]\t*$DIVIDER*\t\t\t\tPods ( $READY/$DESIRED )\t\t\t\t*$DIVIDER"
+fi
+
+if  [ "$READY" -ne "$DESIRED" ]; then
+    printf "[ERROR] All Portworx Pods are not ready, will not proceed with the upgrade!!Please fix Portworx and then try to upgrade.\n"
+    exit 1
+else
+    echo "[INFO] Getting Portworx Installation Status..."
+    PX_POD=$(kubectl get pods -l name=portworx -n kube-system -o custom-columns=":metadata.name" | awk 'END{print}')
+    if ! STATUS=$(kubectl exec $PX_POD -n kube-system -- /opt/pwx/bin/pxctl status --json | jq -r '.status'); then
+        echo "[ERROR] Couldn't get portworx cluster status."
+        exit 1
+    elif [ "$STATUS" == "STATUS_OK" ]; then
+        ds_status=($(kubectl describe ds portworx -n kube-system | grep "Pods Status" | cut -d ":" -f 2))
+        printf "$HEADER*\t\t\t\tDaemonset Status\t\t\t*\n* portworx\t[ ${ds_status[*]} ]\t*$DIVIDER*\t\t\t\tPods ( $READY/$DESIRED )\t\t\t\t*\n"
+        kubectl get pods -l name=portworx -n kube-system | awk 'NR>1 { print "* "$1"\t\t\t [ "$3"\t"$2"\t"$5" ]\t*"  }'
+        printf "$DIVIDER*\t\t\tPortworx Status: $STATUS\t\t\t*$DIVIDER"
+    else
+        echo "[INFO] Portworx Status: $STATUS"
+        exit 1
+    fi
+fi
+
 
 echo "[INFO] Kube Config Path: $CONFIGPATH"
 export KUBECONFIG=$CONFIGPATH
@@ -28,16 +63,11 @@ if [ "$VERSION" == "" ]; then
 fi
 
 $CMD history portworx
-if [[ $? -ne 0 ]]; then
-    echo "[ERROR] Portworx Installation not found! Can't upgrade!!"
-    exit 1
-
 $CMD repo add community https://raw.githubusercontent.com/IBM/charts/master/repo/community
 $CMD repo update
 $CMD get values portworx -n default > /tmp/values.yaml
 sed -i -E -e 's@PX_IMAGE=icr.io/ext/portworx/px-enterprise:.*$@PX_IMAGE=icr.io/ext/portworx/px-enterprise:'"$IMAGE_VERSION"'@g' /tmp/values.yaml
 $CMD upgrade portworx community/portworx -f /tmp/values.yaml --set imageVersion=$IMAGE_VERSION --wait --timeout 5h0m0s
-
 if [[ $? -eq 0 ]]; then
     echo "[INFO] Successfully Upgraded!!"
 else
