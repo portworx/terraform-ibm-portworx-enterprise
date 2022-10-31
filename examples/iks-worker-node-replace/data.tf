607105627239dce200478000bdf9b7faca65bd18
Sumit Das <sudas@purestorage.com>
2022-10-31 14:04:09 +0530

    add: `data_source` for workers query
    Signed-off-by: Sumit Das <sudas@purestorage.com>

--- examples/iks-worker-node-replace/data.tf
+++ examples/iks-worker-node-replace/data.tf
@@ -7,6 +7,11 @@ data "ibm_container_cluster_config" "cluster" {
   cluster_name_id = data.ibm_container_vpc_cluster.cluster.id
   config_dir      = path.root
 }
+# Get all the workers from the clusters
+data "ibm_container_vpc_cluster" "cluster" {
+  name              = var.iks_cluster_name
+  resource_group_id = data.ibm_resource_group.resource_group.id
+}
 # Local variables, NOTE: Always get the list of workers into a local varibale first and then pass this local varibale to the `ibm_container_vpc_worker.resource` resource
 # DO NOT USE the `data.ibm_container_vpc_cluster.cluster.workers` variable directly in the `ibm_container_vpc_worker.worker` resource
 locals {
