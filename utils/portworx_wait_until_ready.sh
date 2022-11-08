83586774af77611c750f90859f16682b421bae42
Sumit Das <sudas@purestorage.com>
2022-11-08 20:04:32 +0530

    removed: DEBUG line
    Signed-off-by: Sumit Das <sudas@purestorage.com>

--- utils/portworx_wait_until_ready.sh
+++ utils/portworx_wait_until_ready.sh
@@ -8,15 +8,14 @@ STATUS=""
 SLEEP_TIME=30
 LIMIT=15
 RETRIES=0
-sleep 90
+# sleep 90
 
 while [ "$RETRIES" -le "$LIMIT" ]
 do
-    echo "kubectl get storagecluster ${PX_CLUSTER_NAME} -n ${NAMESPACE} -o jsonpath='{.items[*].status.phase}'"
-    STATUS=$(kubectl get storagecluster ${PX_CLUSTER_NAME} -n ${NAMESPACE} -o jsonpath='{.items[*].status.phase}')
-    if [ "$STATUS" == "Online" ]; then
-        CLUSTER_ID=$(kubectl get storagecluster ${PX_CLUSTER_NAME} -n ${NAMESPACE} -o jsonpath='{.items[*].status.clusterUid}')
-        printf "[SUCCESS] Portworx Storage Cluster is Online. Cluster ID: ($CLUSTER_ID)\n"
+    STATUS=$(kubectl get storagecluster ${PX_CLUSTER_NAME} -n ${NAMESPACE} -o yaml | grep phase | cut -d ":" -f2)
+    if [ "${STATUS// /}" == "Online" ]; then
+        CLUSTER_ID=$(kubectl get storagecluster ${PX_CLUSTER_NAME} -n ${NAMESPACE} -o yaml | grep clusterUid | cut -d ":" -f2)
+        printf "[SUCCESS] Portworx Storage Cluster is Online. Cluster ID: (${CLUSTER_ID// /})\n"
         break
     fi
     printf "[INFO] Portworx Storage Cluster Status: [ $STATUS ]\n"
